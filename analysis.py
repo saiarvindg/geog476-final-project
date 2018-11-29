7b66de1547756f4520b1cd74fa8119ec9e210164
Sai Arvind <sai.arvindg@gmail.com>
2018-11-29 15:16:01 -0500

    updated analysis.py

--- analysis.py
+++ analysis.py
@@ -1,11 +1,15 @@
-import GeoPandas as gp
+import pandas as pd
+import geopandas as gp
 
 class Analysis:
 	stateNameToCensusTractNum = {'RI':44}
 	nlcdComponents = []
 	
-	def __init__(self, fileName : str) -> None:
-		self.fileName = fileName
+	def __init__(self, censusTractFile, landCoverFile, imperviousFile) -> None:
+		self.censusTractFile = censusTractFile
+		self.landCoverFile = landCoverFile
+		self.imperviousFile = imperviousFile
+		self.censusTractsGDF = self.getCensusTracts()
 	
 	# NOTE: I think we should leave any graphing up to the user (i.e. just give them the data frame)
 	def performPearsonAnalysisForState(self, state : str) -> (gp.GeoDataFrame,str, int):
@@ -13,7 +17,6 @@ class Analysis:
 
 		if state in stateNameToCensusTractNum:
 			# extract census tracts and assign as instance
-			self.censusTractsGDF = self.getCensusTracts()
 			calcImperviousSurfaceCoverPercentage()
 			calcLandCoverPercentages()
 			self.landCoverWithHighestR = getLandCoverTypeWithHighestR()
@@ -25,8 +28,10 @@ class Analysis:
 	
 	def getCensusTracts(self) -> gp.GeoDataFrame:
 		""" Get the census tracts for each state """
-		# TODO: use GeoPandas to select census tracts for the states
-		return  # TODO: return GeoPandas data frame
+		# TODO: use rasterio and GeoPandas to select census tracts for the states
+		stateCensusTract = gp.read_file(r"../Tract_2010Census_DP1/Tract_2010Census_DP1.shp")
+		stateCensusTract.head()
+		return stateCensusTract
 
 	def calcPopulationDensity(self) -> None:
 		""" Calcluate the populaiton density and add a column to the censusTractsGDF """
@@ -47,11 +52,11 @@ class Analysis:
 		For each land cover type - loop through, calculate, and add the percentage
 		of each land cover type to the censusTractsGDF
 		"""
-
 		for landCover in ncldComponents:
 			#TODO: calculate the percentage of the landCover for each census tract
 
 			#TODO: add the land o column to census tract GDF
+			pass
 	
 	def calcPearsonCorrelationForImperviousLandCover() -> None:
 		""" 
@@ -62,11 +67,16 @@ class Analysis:
 
 		#TODO: add the land cover type and the r value to a dictionary in the class
 		if self.landCoverPearsonCorrelations == None:
+			#TODO
+			"""
 			self.landCoverPearsonCorrelations = {
-				'impervious': #TODO: fill in the r value
+				'impervious': fill in the r value
 			}
+			"""
+			pass
 		else:
-			self.landCoverPearsonCorrelations['impervious'] = #TODO: fill in the r value
+			# self.landCoverPearsonCorrelations['impervious'] = #TODO: fill in the r value
+			pass
 	
 	def calcPearsonCorrelationForNLCDComponents() -> None:
 		""" 
@@ -78,11 +88,15 @@ class Analysis:
 
 			#TODO: add the land cover type and the r value to a dictionary in the class
 			if self.landCoverPearsonCorrelations == None:
+				#TODO
+				"""
 				self.landCoverPearsonCorrelations = {
-					landCover : #TODO: fill in the r value
+					landCover : fill in the r value
 				}
+				"""
 			else:
-				self.landCoverPearsonCorrelations[landCover] = #TODO: fill in the r value
+				# self.landCoverPearsonCorrelations[landCover] = #TODO: fill in the r value
+				pass
 
 	def getLandCoverTypeWithHighestR(self) -> (str, int):
 		""" 
@@ -90,11 +104,13 @@ class Analysis:
 		and r value with the highest correlation
 		"""
 		#TODO: get the land cover type with the highest correlation
+		"""
 		return (
 			#TODO: insert land cover type
 			,
 			#TODO: insert land cover type's r value
 			)
+		"""
 
 	@staticmethod
 	def getCensusTractNumberForState(state : str):
