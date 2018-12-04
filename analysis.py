fdefe3ccbe0200e9787432df033337d72c7057c0
Devin Simmons <39880645+devinsimmons@users.noreply.github.com>
2018-12-04 10:51:31 -0500

    rerpojecting census tracts
    adjusted sai's method to reproject the census tract dataframe so that it matches the NLCD raster

--- analysis.py
+++ analysis.py
@@ -72,7 +72,8 @@ class Analysis:
 			# select only the census tract rows we need
 			# creating filter
 			self.stateCensusTractGDF = self.stateCensusTractGDF[self.stateCensusTractGDF['GEOID10'].str.startswith(str(self.censusTractNum))]
-			self.stateCensusTractGDF = self.stateCensusTractGDF.to_crs({'init': 'epsg:5070'})
+			#Reprojected. crs data is shown as a proj 4 string -Devin
+			self.stateCensusTractGDF = self.stateCensusTractGDF.to_crs('+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
 		else:
 			raise TypeError('Please pass in a valid state abbrevation')
 
