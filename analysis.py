eabd865c1a8374610e70cdabdd18efb3bca2e166
Sai Arvind <sai.arvindg@gmail.com>
2018-12-05 11:40:41 -0500

    cleaned up structure and added multiple linear regression analysis

--- analysis.py
+++ analysis.py
@@ -1,5 +1,3 @@
-
-
 import pandas as pd
 import geopandas as gp
 import numpy as np
@@ -9,8 +7,9 @@ import rasterstats as rs
 import scipy.stats
 import matplotlib
 import numpy as np
+import sklearn.linear_model
 
-stateNameToCensusTractNum = {'RI':44}
+stateNameToCensusTractNum = {'RI':'44','NY':'36','FL':'12','WI':'55','CA':'06','NE':'31'}
 nlcdComponents = []
 
 class Analysis:
@@ -38,12 +37,91 @@ class Analysis:
 
 	# NOTE: I think we should leave any graphing outside this class (i.e. just give them the data frame)
 	#Sai
-	def performPearsonAnalysis(self) -> gp.GeoDataFrame:
+	def performPearsonAnalysis(self) -> None:
 		""" Performs the analysis and return the GeoDataFrame and R value """
-		print("Performing Pearson analysis for census tract number: " + str(self.censusTractNum))
-		self.calcPopDensityAndLandCoverPercents()
+		print("Performing Pearson analysis for census tract number: " + self.censusTractNum)
+		self.calcPearsonForImpervious()
+		self.calcPearsonForLandCover()
+
+	def performMultLinRegAnalysis(self) -> None:
+		""" Performs the analysis and return the GeoDataFrame and R value """
+		print("Performing Multiple Linear Regression analysis for census tract number: " + self.censusTractNum)
+		self.calcMultLinReg()	
+	
+	
+	def calcPearsonForImpervious(self) -> None:
+		#read these variables into lists so that scipy can determine statistics
+		pop_density_list = self.stateCensusTractGDF['pop_density'].tolist()
+		impervious_mean_list = self.stateCensusTractGDF['impervious_mean'].tolist()
+		#need to remove nan values
+		pop_density_list = np.array(pop_density_list)
+		self.pop_density_list = np.nan_to_num(pop_density_list)
+		impervious_mean_list = np.array(impervious_mean_list)
+		impervious_mean_list = np.nan_to_num(impervious_mean_list)
+		#linear regression stats
+		slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(self.pop_density_list, impervious_mean_list)
+
+		self.plotRegression(self.pop_density_list, impervious_mean_list, r_value, intercept, slope, "impervious")
+		plt.show()
+
+	def calcPearsonForLandCover(self) -> None:
+		for i in self.land_covers:
+			if i in self.stateCensusTractGDF.columns:
+				#calculate percent land cover
+				#need to divide the land cover columns by the nlcd count col to get the percentage land cover
+				lc_mean_list = self.stateCensusTractGDF[i].tolist()
+				lc_mean_list = np.array(lc_mean_list)
+				lc_mean_list = np.nan_to_num(lc_mean_list)
+				slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(self.pop_density_list, 
+                                                                                     lc_mean_list)
+				self.plotRegression(self.pop_density_list, lc_mean_list, r_value, intercept, slope, i)
+				plt.show()
+
+	def calcMultLinReg(self) -> None:
+		#read these variables into lists so that scipy can determine statistics
+		pop_density_list = self.stateCensusTractGDF['pop_density'].tolist() # Y
+		impervious_mean_list = self.stateCensusTractGDF['impervious_mean'].tolist() # X1
+		developed_list = self.stateCensusTractGDF['developed'].tolist() # X2
+		planted_list = self.stateCensusTractGDF['planted'].tolist() # X3
 		
-		return self.stateCensusTractGDF
+		#need to remove nan values
+		pop_density_list = np.array(pop_density_list)
+		self.pop_density_list = np.nan_to_num(pop_density_list)
+
+		impervious_mean_list = np.array(impervious_mean_list)
+		impervious_mean_list = np.nan_to_num(impervious_mean_list)
+
+		developed_list = np.array(developed_list)
+		developed_list = np.nan_to_num(developed_list)
+
+		planted_list = np.array(planted_list)
+		planted_list = np.nan_to_num(planted_list)
+
+		print("pop density: " + str(len(self.pop_density_list)))
+		print("impervious_mean_list: " + str(len(impervious_mean_list)))
+		print("developed_list: " + str(len(developed_list)))
+		print("planted_list: " + str(len(planted_list)))
+
+
+		#linear regression stats
+		landcovers = [impervious_mean_list, developed_list, planted_list]
+		regr = sklearn.linear_model.LinearRegression()
+		# regr = regr.fit(landcovers, self.pop_density_list)
+		regr = regr.fit(self.stateCensusTractGDF[["impervious_mean","developed","planted"]], self.pop_density_list)
+		r2_value = regr.score(landcovers, self.pop_density_list)
+		print("mult. lin. reg r2 value: " + str(r2_value))
+		intercept = regr.intercept_
+		print("mult. lin. reg intercept: " + str(intercept))
+		slope = regr.coeff_
+		print("mult. lin. reg. coefficient: "  + str(slope))
+		regr.predict(landcovers)
+		print("mult. lin. reg get_params: " + str(regr.get_params()))
+		pred = regr.predict(landcovers)
+		print("muilt. lin. reg prediction: "  + str(pred))
+
+		# self.plotRegression(self.pop_density, None, r2_value, intercept, slope, None, False)
+		# plt.show()
+	
 
 	#Sai
 	def setCensusTractDataFrameForState(self, state : str):
@@ -66,13 +144,13 @@ class Analysis:
 			self.stateCensusTractGDF = self.nationalCensusTractsGDF[colNames]
 			# select only the census tract rows we need
 			# creating filter
-			self.stateCensusTractGDF = self.stateCensusTractGDF[self.stateCensusTractGDF['GEOID10'].str.startswith(str(self.censusTractNum))]
+			self.stateCensusTractGDF = self.stateCensusTractGDF[self.stateCensusTractGDF['GEOID10'].str.startswith(self.censusTractNum)]
 
 			#crs data is shown as a proj 4 string 
 
 			#Reprojected. crs data is shown as a proj 4 string -Devin
-
 			self.stateCensusTractGDF = self.stateCensusTractGDF.to_crs('+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
+			self.calcPopDensityAndLandCoverPercents()
 		else:
 			raise TypeError('Please pass in a valid state abbrevation')
 
@@ -99,19 +177,6 @@ class Analysis:
 		self.stateCensusTractGDF['pop_density'] = self.stateCensusTractGDF['DP0010001']/self.stateCensusTractGDF['ALAND10']
 		self.impervious_stats = rs.zonal_stats(self.stateCensusTractGDF, self.imperviousFile, prefix = "impervious_", stats = 'mean', geojson_out = True)
 		self.stateCensusTractGDF = gp.GeoDataFrame.from_features(self.impervious_stats)
-		#read these variables into lists so that scipy can determine statistics
-		pop_density_list = self.stateCensusTractGDF['pop_density'].tolist()
-		impervious_mean_list = self.stateCensusTractGDF['impervious_mean'].tolist()
-		#need to remove nan values
-		pop_density_list = np.array(pop_density_list)
-		self.pop_density_list = np.nan_to_num(pop_density_list)
-		impervious_mean_list = np.array(impervious_mean_list)
-		impervious_mean_list = np.nan_to_num(impervious_mean_list)
-		#linear regression stats
-		slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(self.pop_density_list, impervious_mean_list)
-
-		self.plotRegression(self.pop_density_list, impervious_mean_list, r_value, intercept, slope, "impervious")
-		plt.show()
 
 	#Devin
 	def calcNLCDComponentsPercentages(self) -> None:
@@ -142,29 +207,25 @@ class Analysis:
 		for i in self.land_covers:
 			if i in self.stateCensusTractGDF.columns:
 				#calculate percent land cover
+				#divide the land cover columns by the nlcd count col to get the percentage land cover
 				self.stateCensusTractGDF[i] = 100 * self.stateCensusTractGDF[i]/self.stateCensusTractGDF['nlcdcount']
-				lc_mean_list = self.stateCensusTractGDF[i].tolist()
-				lc_mean_list = np.array(lc_mean_list)
-				lc_mean_list = np.nan_to_num(lc_mean_list)
-				slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(self.pop_density_list, 
-                                                                                     lc_mean_list)
-				self.plotRegression(self.pop_density_list, lc_mean_list, r_value, intercept, slope, i)
-				plt.show()
-		#TODO: i need to divide the land cover columns by the nlcd count col to get the percentage land cover
 
-	def plotRegression(self, pop_density, percent_landcover, r, intercept, slope, lc_type):
+	def plotRegression(self, pop_density, percent_landcover, r, intercept, slope, lc_type, pearson=True):
 		plt.style.use('seaborn-darkgrid')
 
-		#scatter plot of points
-		plt.scatter(pop_density, percent_landcover, color = '#31a354',
-                    edgecolors = 'black')
-		line = pop_density * slope + intercept
-		plt.plot(pop_density, line, 'r', zorder = 5, 
-                 label='y = {:.2f}x + {:.2f}, R$^2$ = {:.2f}'.format(slope, intercept, r**2), 
-                 color = '#377eb8', linewidth = 2.5)
-		plt.legend()
-		plt.xlabel('People per square mile')
-		plt.ylabel('Percent {}'.format(lc_type))
-		plt.title('Percent {} vs. Pop Density in {}'.format(lc_type, self.state))
-		#plt.savefig(r'C:\Users\simmonsd\Documents\GitHub\geog476-final-project\figures\{}_{}_linreg.png'.format(self.state, lc_type), dpi=250)
-		plt.savefig('../figures/{}_{}_linreg.png'.format(self.state, lc_type), dpi=250)
+		if pearson:
+			#scatter plot of points
+			plt.scatter(pop_density, percent_landcover, color = '#31a354',
+						edgecolors = 'black')
+			line = pop_density * slope + intercept
+			plt.plot(pop_density, line, 'r', zorder = 5, 
+					label='y = {:.2f}x + {:.2f}, R$^2$ = {:.2f}'.format(slope, intercept, r**2), 
+					color = '#377eb8', linewidth = 2.5)
+			plt.legend()
+			plt.xlabel('People per square mile')
+			plt.ylabel('Percent {}'.format(lc_type))
+			plt.title('Percent {} vs. Pop Density in {}'.format(lc_type, self.state))
+			plt.savefig('./figures/{}_{}_pearson.png'.format(self.state, lc_type), dpi=250)
+		else:
+			pass
+			# plt.savefig('./figures/{}_linreg.png'.format(self.state), dpi=250)
