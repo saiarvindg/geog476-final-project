82af8a52ef836f0191cbf9eac2fe99114dc2747d
Sai Arvind <sai.arvindg@gmail.com>
2018-12-05 12:28:25 -0500

    fixed multiple lin reg

--- analysis.py
+++ analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import geopandas as gp
 import numpy as np
-import os.path as path
+import os
 import matplotlib.pyplot as plt
 import rasterstats as rs
 import scipy.stats
@@ -17,7 +17,7 @@ class Analysis:
 	def __init__(self, censusTractFile : str, landCoverFile : str, imperviousFile : str) -> None:
 		from functools import reduce 
 		# check if all files exist
-		if reduce(lambda x,y: (x and path.isfile(y)), [censusTractFile, landCoverFile, imperviousFile], True):
+		if reduce(lambda x,y: (x and os.path.isfile(y)), [censusTractFile, landCoverFile, imperviousFile], True):
 			self.censusTractFile = censusTractFile
 			self.landCoverFile = landCoverFile
 			self.imperviousFile = imperviousFile
@@ -61,7 +61,7 @@ class Analysis:
 		#linear regression stats
 		slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(self.pop_density_list, impervious_mean_list)
 
-		self.plotRegression(self.pop_density_list, impervious_mean_list, r_value, intercept, slope, "impervious")
+		self.plotPearsonRegression(self.pop_density_list, impervious_mean_list, r_value, intercept, slope, "impervious")
 		plt.show()
 
 	def calcPearsonForLandCover(self) -> None:
@@ -74,53 +74,24 @@ class Analysis:
 				lc_mean_list = np.nan_to_num(lc_mean_list)
 				slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(self.pop_density_list, 
                                                                                      lc_mean_list)
-				self.plotRegression(self.pop_density_list, lc_mean_list, r_value, intercept, slope, i)
+				self.plotPearsonRegression(self.pop_density_list, lc_mean_list, r_value, intercept, slope, i)
 				plt.show()
 
 	def calcMultLinReg(self) -> None:
 		#read these variables into lists so that scipy can determine statistics
-		pop_density_list = self.stateCensusTractGDF['pop_density'].tolist() # Y
-		impervious_mean_list = self.stateCensusTractGDF['impervious_mean'].tolist() # X1
-		developed_list = self.stateCensusTractGDF['developed'].tolist() # X2
-		planted_list = self.stateCensusTractGDF['planted'].tolist() # X3
-		
-		#need to remove nan values
+		pop_density_list = self.stateCensusTractGDF['pop_density'].tolist()
 		pop_density_list = np.array(pop_density_list)
 		self.pop_density_list = np.nan_to_num(pop_density_list)
 
-		impervious_mean_list = np.array(impervious_mean_list)
-		impervious_mean_list = np.nan_to_num(impervious_mean_list)
-
-		developed_list = np.array(developed_list)
-		developed_list = np.nan_to_num(developed_list)
-
-		planted_list = np.array(planted_list)
-		planted_list = np.nan_to_num(planted_list)
-
-		print("pop density: " + str(len(self.pop_density_list)))
-		print("impervious_mean_list: " + str(len(impervious_mean_list)))
-		print("developed_list: " + str(len(developed_list)))
-		print("planted_list: " + str(len(planted_list)))
-
-
 		#linear regression stats
-		landcovers = [impervious_mean_list, developed_list, planted_list]
+		# landcovers = [impervious_mean_list, developed_list, planted_list]
+		landcovers = self.stateCensusTractGDF[["impervious_mean","developed","planted"]]
 		regr = sklearn.linear_model.LinearRegression()
-		# regr = regr.fit(landcovers, self.pop_density_list)
-		regr = regr.fit(self.stateCensusTractGDF[["impervious_mean","developed","planted"]], self.pop_density_list)
+		regr = regr.fit(landcovers, self.pop_density_list)
 		r2_value = regr.score(landcovers, self.pop_density_list)
-		print("mult. lin. reg r2 value: " + str(r2_value))
-		intercept = regr.intercept_
-		print("mult. lin. reg intercept: " + str(intercept))
-		slope = regr.coeff_
-		print("mult. lin. reg. coefficient: "  + str(slope))
-		regr.predict(landcovers)
-		print("mult. lin. reg get_params: " + str(regr.get_params()))
 		pred = regr.predict(landcovers)
-		print("muilt. lin. reg prediction: "  + str(pred))
 
-		# self.plotRegression(self.pop_density, None, r2_value, intercept, slope, None, False)
-		# plt.show()
+		self.plotMultLinReg(pred, self.pop_density_list, r2_value, regr.intercept_, regr.coef_)
 	
 
 	#Sai
@@ -210,22 +181,33 @@ class Analysis:
 				#divide the land cover columns by the nlcd count col to get the percentage land cover
 				self.stateCensusTractGDF[i] = 100 * self.stateCensusTractGDF[i]/self.stateCensusTractGDF['nlcdcount']
 
-	def plotRegression(self, pop_density, percent_landcover, r, intercept, slope, lc_type, pearson=True):
+	def plotPearsonRegression(self, x, y, r, intercept, slope, lc_type=None, multilinreg=False):
+		if not os.path.isdir('./figures'):
+			os.makedirs('./figures')
+		
 		plt.style.use('seaborn-darkgrid')
-
-		if pearson:
-			#scatter plot of points
-			plt.scatter(pop_density, percent_landcover, color = '#31a354',
-						edgecolors = 'black')
-			line = pop_density * slope + intercept
-			plt.plot(pop_density, line, 'r', zorder = 5, 
-					label='y = {:.2f}x + {:.2f}, R$^2$ = {:.2f}'.format(slope, intercept, r**2), 
-					color = '#377eb8', linewidth = 2.5)
-			plt.legend()
+		#scatter plot of points
+		plt.scatter(x, y, color = '#31a354',
+					edgecolors = 'black')
+		line = x * slope + intercept
+		plt.plot(x, line, 'r', zorder = 5, 
+				label='y = {:.2f}x + {:.2f}, R$^2$ = {:.2f}'.format(slope, intercept, r**2), 
+				color = '#377eb8', linewidth = 2.5)
+		plt.legend()
+		
+		if multilinreg:
+			plt.xlabel('Predicted Population Density')
+			plt.ylabel('Acutal Population Density')
+			plt.title('Multiple Linear Regression for {}'.format(self.state))
+			plt.savefig('./figures/{}_mutli_linreg.png'.format(self.state), dpi=250)
+		else:
 			plt.xlabel('People per square mile')
 			plt.ylabel('Percent {}'.format(lc_type))
 			plt.title('Percent {} vs. Pop Density in {}'.format(lc_type, self.state))
 			plt.savefig('./figures/{}_{}_pearson.png'.format(self.state, lc_type), dpi=250)
-		else:
-			pass
-			# plt.savefig('./figures/{}_linreg.png'.format(self.state), dpi=250)
+
+
+	def plotMultLinReg(self, predicted, actual, multi_linreg_r2, multi_linreg_intercept, multi_linreg_coeffs):
+		slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(predicted, actual)
+		self.plotPearsonRegression(predicted, actual, r_value, intercept, slope, None, True)
+		plt.show()
